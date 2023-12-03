d4c9f749e0ed6b3a8b4365d4a4c9ca9d4ce87e8d
Ltgds <Ltgds@outlook.com>
2023-12-03 21:55:25 +0800

    3. dict和obj

---
+++ godis.go
@@ -0,0 +1,18 @@
+package main
+
+type CmdType = byte
+
+const (
+	COMMAND_UNKNOWN CmdType = 0x00
+	COMMAND_INLINE  CmdType = 0x01
+	COMMAND_BULK    CmdType = 0x02
+)
+
+const (
+	GODIS_IO_BUF     int = 1024 * 16
+	GODIS_MAX_BULK   int = 1024 * 4
+	GODIS_MAX_INLINE int = 1024 * 4
+)
+
+type GodisDB struct {
+}
